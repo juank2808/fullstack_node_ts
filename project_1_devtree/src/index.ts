efdba34b4040c4c43eaf3e2860de6e379121a480
juank2808 <webjcaf022014@gmail.com>
2025-02-14 00:18:24 +0100

    Separating the creation of the server into its own file

--- project_1_devtree/src/index.ts
+++ project_1_devtree/src/index.ts
@@ -1,13 +1,6 @@
 //ESM sintaxis
-import express, { Request, Response } from 'express';
-
-const app = express();
-
-app.get('/', (req :Request, res: Response)=>{
-    res.send("HOLA");
-});
-
+import server from './server';
 // const port = process.env.PORT || 4000;
-app.listen(4000, ()=> {
+server.listen(4000, ()=> {
     console.log("Server is running...");
 });
