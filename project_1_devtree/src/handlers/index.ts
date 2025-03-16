174d63754080f60045696f7141e45eec5d0fb670
juank2808 <webjcaf022014@gmail.com>
2025-03-17 00:17:15 +0100

    feature/ userExist

--- project_1_devtree/src/handlers/index.ts
+++ project_1_devtree/src/handlers/index.ts
@@ -1,7 +1,21 @@
 import User from "../models/User";
 import { Request, Response } from "express";
-export const createAccount  = async (req:Request, res:Response) => {
-    const user = new User(req.body);
-    user.save();
-    res.send("USER_CREATED");
+
+export const createAccount = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { email } = req.body;
+
+        const userExist = await User.findOne({ email });
+        if (userExist) {
+            res.status(400).json({ error: "USER_EXIST" });
+            return; // Asegura que la función termine aquí
+        }
+
+        const user = new User(req.body);
+        await user.save();
+
+        res.status(201).json({ message: "USER_CREATED" });
+    } catch (error) {
+        res.status(500).json({ error: "INTERNAL_SERVER_ERROR" });
+    }
 };
